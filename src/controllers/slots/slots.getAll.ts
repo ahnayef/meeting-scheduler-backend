e75bb798bc085333f5f6a8e272bdf913608aa564
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2024-12-09 08:02:35 +0600

    OK

--- src/controllers/slots/slots.getAll.ts
+++ src/controllers/slots/slots.getAll.ts
@@ -18,12 +18,18 @@ const getSlots = async (req: any, res: any) => {
             FROM slot WHERE user_id = ? AND is_booked = FALSE`,[user_id]
         );
 
+
+        const [bookedSlots]: any = await db.query(
+            `SELECT slot_id, user_id, date, start_tm, end_tm, is_booked 
+            FROM slot WHERE user_id = ? AND is_booked = TRUE`,[user_id]
+        );
+
         // const userName: string = = `SELECT name from user`
 
         
 
         return res.status(200).json({
-            data: slots,
+            data: {slots, bookedSlots},
         });
     } catch (error) {
         console.error(error);
